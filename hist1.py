1933a2c7811a247b22048dc1da9ccd318b746471
stats-dev <hohun94@naver.com>
2023-11-27 19:15:00 +0900

    matplotlib and hist updates

---
+++ hist1.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
---
+++ matplotlib.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Change the line plot below to a scatter plot
+plt.plot(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+plt.scatter(gdp_cap, life_exp)
+
+# Show plot
+plt.show()
+
+
+## two with scatter
