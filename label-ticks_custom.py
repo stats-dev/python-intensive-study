1b793c2259f5829ac26ab002f045c07ebbdad1ca
stats-dev <hohun94@naver.com>
2023-11-28 10:08:16 +0900

    coloring with c option in plt scatter

--- label-ticks_custom.py
+++ label-ticks_custom.py
@@ -41,3 +41,17 @@ plt.xticks(tick_val, tick_lab)
 # After customizing, display the plot
 plt.show()
 
+
+## col for coloring custom.
+# Specify c and alpha inside plt.scatter()
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Show the plot
+plt.show()
