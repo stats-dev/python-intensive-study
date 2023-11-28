c6b514914c34c0d7267dce842b5c68c8873b2c7d
stats-dev <hohun94@naver.com>
2023-11-28 09:50:15 +0900

    hist and customization updates

--- hist2.py
+++ hist2.py
@@ -14,3 +14,18 @@ plt.show()
 plt.clf()
 
 # bin을 늘려두면, 좀 더  촘촘하다. 그러나 무조건 늘린다고 좋지 않다. 패턴이 잘 보인다.
+
+##bins with present and past
+# Histogram of life_exp, 15 bins
+plt.hist(life_exp, bins=15)
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Histogram of life_exp1950, 15 bins
+plt.hist(life_exp1950, bins=15)
+
+# Show and clear plot again
+plt.show()
+plt.clf()
---
+++ label-ticks_custom.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+
+## bins with xlabel, ylabel, title
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+# Add title
+plt.title(title)
+
+# After customizing, display the plot
+plt.show()
+
+## xticks
+
+# Custom: Scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks(tick_val, tick_lab)
+
+# After customizing, display the plot
+plt.show()
+
