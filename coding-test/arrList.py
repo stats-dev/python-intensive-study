3ee640d6adb9073f55949b64838ffa4c374ffb31
stats-dev <hohun94@naver.com>
2023-11-30 12:30:30 +0900

    list and pandas series, dataframe

--- coding-test/arrList.py
+++ coding-test/arrList.py
@@ -16,3 +16,11 @@ myList.insert(2, 1234)
 print(myList) # [1, 2, 1234, 3, 4, 5]
 
 # 삭제
+## pop() 특정 위치 데이터 팝팝!
+myList = [1,2,3,4,5]
+pop_ele = myList.pop(2) #반환하는 2번째 인덱스 값
+print(f"전체 {myList}, 반환 {pop_ele}")
+print(pop_ele)
+
+## remove() 특정 데이터 삭제: 위치아님
+
