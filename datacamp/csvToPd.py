a88ea8543ea7280750e058e1471cd272cacc84a1
stats-dev <hohun94@naver.com>
2023-11-30 10:16:38 +0900

    index_col updates

--- datacamp/csvToPd.py
+++ datacamp/csvToPd.py
@@ -7,7 +7,16 @@ cars = pd.read_csv('./cars.csv')
 
 # Print out cars
 print(cars)
+#   Unnamed: 0  cars_per_cap        country  drives_right
+# 0         US           809  United States          True
+# 1        AUS           731      Australia         False
+# 2        JPN           588          Japan         False
+# 3         IN            18          India         False
+# 4         RU           200         Russia          True
+# 5        MOR            70        Morocco          True
+# 6         EG            45          Egypt          True
 
+## 저걸 지워버리자.. index_col=0
 # Import pandas as pd
 import pandas as pd
 
