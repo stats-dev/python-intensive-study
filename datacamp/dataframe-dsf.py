66ac167c1a6738129f54a8140473bfc61fe3b8de
stats-dev <hohun94@naver.com>
2023-11-30 09:34:30 +0900

    pandas dataframe and index

--- datacamp/dataframe-dsf.py
+++ datacamp/dataframe-dsf.py
@@ -27,4 +27,34 @@ print(cars)
 
 # 이번에는 인덱스를 country로 해볼까?
 cars = pd.DataFrame(my_dict, index=my_dict['country'])
-print(my_dict['drives_right'])
+# 아쉽다.
+#  that the columns of cars can be of different types. This was not possible with 2D NumPy arrays!
+
+## 인덱스 뽑아내는 다른 방법
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+# Print cars again
+print(cars)
+
+#            country  drives_right  cars_per_cap
+# US   United States          True           809
+# AUS      Australia         False           731
+# JPN          Japan         False           588
+# IN           India         False            18
+# RU          Russia          True           200
+# MOR        Morocco          True            70
+# EG           Egypt          True            45
