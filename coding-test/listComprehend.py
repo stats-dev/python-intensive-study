212540856bcc8f3a22e8cea4c66c9e1ea746e24f
stats-dev <hohun94@naver.com>
2023-12-02 17:20:17 +0900

    rearrange file

--- coding-test/listComprehend.py
+++ coding-test/listComprehend.py
@@ -21,3 +21,45 @@ print(examples.count("a"))
 ## reverse true
 examples.sort(reverse=True)
 print(examples) # ['f', 'e', 'd', 'c', 'b', 'a', 'a']
+
+## no sort but O(N^2)
+import time # 시간을 잰다
+
+def bubble_sort(arr): # 버블 정렬로 정렬
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j] ## 이렇게 교체한다.
+    return arr
+
+def do_sort(arr): # sort() 함수로 배열 정렬하기
+    arr.sort()
+    return arr
+
+
+## 시간 측정
+def measure_time(func, arr):
+    start_time = time.time() ## 시작 시간
+    result = func(arr)
+    end_time = time.time() ## 종료 시간
+    return end_time - start_time, result
+
+arr = list(range(10000)) ## 1만개 원소 리스트/배열
+
+# 첫 코드 시간
+buTime, buResult = measure_time(bubble_sort, arr)
+print("첫번째 코드 실행 시간: ", format(buTime, ".10f")) #2초 걸림
+
+## 두번째 코드
+arr = list(range(10000))
+rev_time, rev_result = measure_time(do_sort, arr)
+print("두번째 코드 실행 시간:", format(rev_time, ".10f")) ## 책에 오타가좀 많다. 심각;
+
+# 코드 결과가 동일한지 확인한다.
+print("두 코드 결과 같아요? : ", buResult == rev_result) ## True
+
+
+##2. 배열제어하기
+
+
