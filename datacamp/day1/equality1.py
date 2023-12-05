28807373f237c81047a9b06ae652a7f80536407c
stats-dev <hohun94@naver.com>
2023-12-05 10:26:30 +0900

    equality with int, string, boolean

---
+++ datacamp/day1/equality1.py
@@ -0,0 +1,25 @@
+# Comparison of booleans
+True == False
+
+# Comparison of integers
+-5 * 15 != 75
+
+# Comparison of strings
+"pyscript" == "PyScript"
+
+# Compare a boolean with an integer
+True == 1
+
+
+# Comparison of integers
+x = -3 * 6
+x >= -10
+
+# Comparison of strings
+y = "test"
+"test" <= y
+
+# Comparison of booleans
+True > False
+
+
